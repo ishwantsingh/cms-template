3a657129badc81d38cd16cde5621d939e83c9b29
Ishwant Singh <ishwant8@gmail.com>
2020-07-08 03:48:26 +0530

    FEAT: blog pages working. edited body markdown to text form.

--- content/blogs/2020-07-07_my-first-painting.md
+++ content/blogs/2020-07-07_my-first-painting.md
@@ -2,5 +2,5 @@
 title: My first Painting!
 date: 2020-07-06T22:29:00.362Z
 image: /img/uploads/painting1.jpeg
+body: "This is the first painting I ever created. Oil and otherwise."
 ---
-This is the first painting I ever created. Oil and otherwise.
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,7 +1,5 @@
-import { Component } from "react";
 import styled from "styled-components";
-
-// import { attributes } from "../../content/page3.md";
+import Link from "next/link";
 
 const Container = styled.div`
   display: flex;
@@ -12,16 +10,98 @@ const Container = styled.div`
   font-size: 3rem;
 `;
 
-export default class Blog extends Component {
-  render() {
-    // let { title } = attributes;
-    return (
-      <Container>
-        <h1>Blog</h1>
-        {/* <div className="about-content">
-          <div className="text">{about}</div>
-        </div> */}
-      </Container>
-    );
-  }
+// const importBlog = async () => {
+//   // https://webpack.js.org/guides/dependency-management/#requirecontext
+//   const markdownBlogs = require
+//     .context("../../content/blogs", false, /\.md$/)
+//     .keys()
+//     .map((relativePath) => relativePath.substring(2));
+
+//   return Promise.all(
+//     markdownBlogs.map(async (path) => {
+//       const markdown = await import(`../../content/blogs/${path}`);
+//       return {
+//         ...markdown.attributes,
+//         slug: path.substring(0, path.length - 3),
+//       };
+//     })
+//   );
+// };
+
+// const Blog = ({ blogList }) => (
+//   <Container>
+//     <h1>Blog</h1>
+//     <div className="blogs-container">
+//       {blogList.map((blog) => {
+//         <Link href="/blog/[slug]" as={`/blog/${blog.slug}`}>
+//           <a>
+//             <div key={blog.slug} className="blog">
+//               <img src={blog.image} className="image" />
+//               <div className="text">{blog.body}</div>
+//             </div>
+//           </a>
+//         </Link>;
+//       })}
+//     </div>
+//   </Container>
+// );
+
+// export async function getStaticProps() {
+//   const blogList = await importBlog();
+//   return {
+//     props: {
+//       blogList,
+//     },
+//   };
+// }
+
+// export default Blog;
+
+const importBlogPosts = async () => {
+  // https://webpack.js.org/guides/dependency-management/#requirecontext
+  const markdownFiles = require
+    .context("../../content/blogs", false, /\.md$/)
+    .keys()
+    .map((relativePath) => relativePath.substring(2));
+  console.log(markdownFiles, "mdf");
+  return Promise.all(
+    markdownFiles.map(async (path) => {
+      const markdown = await import(`../../content/blogs/${path}`);
+      console.log(markdown);
+      return {
+        ...markdown.attributes,
+        slug: path.substring(0, path.length - 3),
+      };
+    })
+  );
+};
+
+const Blog = ({ postsList }) => (
+  <Container>
+    <h1>Blog</h1>
+    <div className="blogs-container">
+      {postsList.map((post) => (
+        <div key={post.slug} className="blog">
+          <Link href="/blog/[slug]" as={`/blog/${post.slug}`}>
+            <a>
+              <img src={post.image} className="image" />
+              <div className="text">{post.title}</div>
+            </a>
+          </Link>
+        </div>
+      ))}
+    </div>
+  </Container>
+);
+
+export async function getStaticProps() {
+  const postsList = await importBlogPosts();
+  console.log(postsList);
+  return {
+    props: {
+      postsList,
+    }, // will be passed to the page component as props
+  };
 }
+
+export default Blog;
